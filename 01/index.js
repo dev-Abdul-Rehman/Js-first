cc4cc9dd29e0562075a23bef8494796eef8ec8de
Abdul Rehman <rehman3120025@gmail.com>
2025-08-18 08:40:57 +0000

    change index to variable

--- 01/index.js
+++
@@ -1,2 +0,0 @@
-console.log("Hi, This is my first repository!")
-console.log("and I'm upload to my github profile.")
---
+++ 01/variable.js
@@ -0,0 +1,7 @@
+var firstName = "Jon Doe"
+let fatherName = "Jane Doe"
+const motherName = "Lina Doe"
+grandFather = "uncle wiss"
+firstName = "Harry"
+fatherName = "Potter"
+console.table([firstName,fatherName,motherName, grandFather])
