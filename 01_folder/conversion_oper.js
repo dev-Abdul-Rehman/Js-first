6f9c027c28b7a0f45df5a06aabe16e15dfb579d5
Abdul Rehman <rehman3120025@gmail.com>
2025-08-18 17:04:15 +0000

    conversion&operators

---
+++ 01_folder/conversion_oper.js
@@ -0,0 +1,37 @@
+let str = "12aaa"
+let str2 = Number(str)
+// console.log(str2);
+// console.log(typeof str2);
+let number = "jfkalf";
+let num2 = String(number);
+// console.log(num2);
+// console.log(typeof num2);
+
+let isTrue = 0;
+let booleanisTrue = Boolean(isTrue)
+
+// console.log(booleanisTrue);
+
+
+
+
+let value = 5;
+ let newValue = value++
+console.log(newValue);
+let x = 4;
+let x2 = ++x;
+console.log(x2);
+
+
+console.log(3 - 3 + "9");
+console.log("4" + 4 + "6");
+console.log(true + true);
+console.log("4" - "5" + 6);
+console.log(4 - 6 + "5");
+console.log("4" + 7 - 7);
+
+
+
+
+
+
